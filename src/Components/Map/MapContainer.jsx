9e5adc4b6969ea7bf92571fb85f536765bbd07e6
Marie <91193548+mariefalchorre@users.noreply.github.com>
2025-09-26 12:06:09 +0200

    Updated arcgis css version + refactored access to the mapView (#4)
    * Updated arcgis css version + refactored access to the mapView
    
    * refactored init map

--- src/Components/Map/MapContainer.jsx
+++ src/Components/Map/MapContainer.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 // arcgis api
 import Point from '@arcgis/core/geometry/Point';
@@ -17,66 +17,65 @@ import './MapContainer.css';
 // Candidate does not need to change this file, or understand the map API used
 
 const MapContainer = ({ setAccidents, location }) => {
-  const [view, setView] = useState(null);
-
-  useEffect(() => {
-    if (view) {
-      view.map.add(layerNVDB);
-      view.whenLayerView(layerNVDB).then((layerView) => {
-        // hash fields for fast lookup
-        const _fieldsHash = [];
-        layerView.layer.fields.forEach((field) => {
-          if (field.domain && field.domain?.type === 'coded-value') {
-            _fieldsHash[field.name] = field.domain;
-          }
-        });
-
-        // return attributes where domain codes changed to domain values
-        const _getAttributesWithDomainValue = (attributes) => {
-          var attributesWithDomainValues = [];
-          for (const [key, value] of Object.entries(attributes)) {
-            attributesWithDomainValues[key] = _fieldsHash[key]?.getName(value) ?? value;
-            if (key.indexOf('__Ny_') > -1) {
-              const newKey = key.replace('__Ny_', '');
-              attributesWithDomainValues[newKey] = value;
-              delete attributesWithDomainValues[key];
-            }
-          }
-          return attributesWithDomainValues;
-        };
-
-        // query for accident data when view is updating
-        reactiveUtils.watch(
-          () => view.updating,
-          (updating) => {
-            if (!updating) {
-              layerView
-                .queryFeatures({
-                  outFields: layerView.availableFields,
-                  geometry: view.extent,
-                  returnGeometry: true,
-                })
-                .then((results) => {
-                  const accidentData = results.features.map((feature) => {
-                    const attr = feature.attributes;
-                    attr.x = Math.trunc(feature.geometry?.x ?? 0);
-                    attr.y = Math.trunc(feature.geometry?.y ?? 0);
-                    return _getAttributesWithDomainValue(attr);
-                  });
-                  setAccidents(accidentData);
-                })
-                .catch((error) => {
-                  console.log('query failed: ', error);
-                });
-            }
-          },
-        );
+  // configure map and add listeners
+  const onViewReady = (view) => {
+    view.map.add(layerNVDB);
+    view.whenLayerView(layerNVDB).then((layerView) => {
+      // hash fields for fast lookup
+      const _fieldsHash = [];
+      layerView.layer.fields.forEach((field) => {
+        if (field.domain && field.domain?.type === 'coded-value') {
+          _fieldsHash[field.name] = field.domain;
+        }
       });
-    }
-  }, [view]);
+
+      // return attributes where domain codes changed to domain values
+      const _getAttributesWithDomainValue = (attributes) => {
+        var attributesWithDomainValues = [];
+        for (const [key, value] of Object.entries(attributes)) {
+          attributesWithDomainValues[key] = _fieldsHash[key]?.getName(value) ?? value;
+          if (key.indexOf('__Ny_') > -1) {
+            const newKey = key.replace('__Ny_', '');
+            attributesWithDomainValues[newKey] = value;
+            delete attributesWithDomainValues[key];
+          }
+        }
+        return attributesWithDomainValues;
+      };
+
+      // query for accident data when view is updating
+      reactiveUtils.watch(
+        () => view.updating,
+        (updating) => {
+          if (!updating) {
+            layerView
+              .queryFeatures({
+                outFields: layerView.availableFields,
+                geometry: view.extent,
+                returnGeometry: true,
+              })
+              .then((results) => {
+                const accidentData = results.features.map((feature) => {
+                  const attr = feature.attributes;
+                  attr.x = Math.trunc(feature.geometry?.x ?? 0);
+                  attr.y = Math.trunc(feature.geometry?.y ?? 0);
+                  return _getAttributesWithDomainValue(attr);
+                });
+                setAccidents(accidentData);
+              })
+              .catch((error) => {
+                console.log('query failed: ', error);
+              });
+          }
+        },
+      );
+    });
+  };
 
   // // Task: goTo location in map
   useEffect(() => {
+    const view = document.querySelector('arcgis-map')?.view;
+
     if (view && location) {
       view.graphics.removeAll();
 
@@ -104,11 +103,10 @@ const MapContainer = ({ setAccidents, location }) => {
   return (
     <arcgis-map
       onarcgisViewReadyChange={(e) => {
-        setView(e.target.view);
+        onViewReady(e.target.view);
       }}
       extent={initExtent}
       basemap={basemapNorway}
-      layers={[layerNVDB]}
       style={{ width: '50%', height: '100%' }}
     >
       <arcgis-zoom></arcgis-zoom>
